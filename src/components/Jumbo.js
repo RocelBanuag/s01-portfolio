c7462cdc735116c23268ed73828983461d9d6ad2
RocelBanuag <93896499+RocelBanuag@users.noreply.github.com>
2022-02-24 17:46:53 +0800

    includes add content about me

--- src/components/Jumbo.js
+++ src/components/Jumbo.js
@@ -6,8 +6,8 @@ import Button from 'react-bootstrap/Button'
 export default function Jumbo() {
     return (
         <Jumbotron>
-            <h1>Hello, world!</h1>
-            <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+            <h1>Hello, My name is Rocel!</h1>
+            <p>My hobbies is watching movies and series.</p>
             <Button variant="primary">Learn more</Button>
         </Jumbotron>
     )
